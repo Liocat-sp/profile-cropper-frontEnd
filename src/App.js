a5d7d5ce585d56310fc47883436e16a584e91724
saurabh <liocatsp.gmail.com>
2020-07-17 14:20:23 +0530

    virsion 0.0.1 is completed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ImageCropper from './ImageCropper/imageCropper';
 
 function App() {
   return (
     <div className="App">
-      
+      <ImageCropper />
     </div>
   );
 }
