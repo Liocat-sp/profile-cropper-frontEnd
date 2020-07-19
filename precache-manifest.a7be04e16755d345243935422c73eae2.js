e5b436f2a4085e2f6f19f7380da6b1fb33fcc3a5
saurabh <liocatsp.gmail.com>
2020-07-19 13:52:24 +0530

    Updates

---
+++ precache-manifest.a7be04e16755d345243935422c73eae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8090c2494c9c34fb15d7364345948b09",
+    "url": "/profile-cropper-frontEnd/index.html"
+  },
+  {
+    "revision": "87c47f84cf5f3f4a0c3c",
+    "url": "/profile-cropper-frontEnd/static/css/main.118bc7db.chunk.css"
+  },
+  {
+    "revision": "f3943e1d5db2adbc5306",
+    "url": "/profile-cropper-frontEnd/static/js/2.7b3a50e7.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/profile-cropper-frontEnd/static/js/2.7b3a50e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c47f84cf5f3f4a0c3c",
+    "url": "/profile-cropper-frontEnd/static/js/main.d7cdf72a.chunk.js"
+  },
+  {
+    "revision": "bf266bfab6b612cac8b7",
+    "url": "/profile-cropper-frontEnd/static/js/runtime-main.da200b7d.js"
+  }
+]);
